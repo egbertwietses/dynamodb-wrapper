e7a092a0f99a39ff0a199b2c86177d67c26989bd
egbertwietses <egbert@pionect.nl>
2015-02-06 12:21:15 +0100

    empty table

--- DynamoDbClientWrapper.php
+++ DynamoDbClientWrapper.php
@@ -22,4 +22,37 @@ class DynamoDbClientWrapper {
 			'secret'	=> $secret
 		));
 	}
+        
+        public function emptyTable($table) {
+                // Get table info
+                $result = $this->client->describeTable(array('TableName' => $table));
+                $keySchema = $result['Table']['KeySchema'];
+
+                foreach ($keySchema as $schema) {
+                        if ($schema['KeyType'] === 'HASH') {
+                            $hashKeyName = $schema['AttributeName'];
+                        }
+                        else if ($schema['KeyType'] === 'RANGE') {
+                            $rangeKeyName = $schema['AttributeName'];
+                        }
+                }
+                // Delete items in the table
+                $scan = $this->client->getIterator('Scan', array('TableName' => $table));
+                foreach ($scan as $item) {
+                        // set hash key
+                        $hashKeyType = array_key_exists('S', $item[$hashKeyName]) ? 'S' : 'N';
+                        $key = array(
+                            $hashKeyName => array($hashKeyType => $item[$hashKeyName][$hashKeyType]),
+                        );
+                        // set range key if defined
+                        if (isset($rangeKeyName)) {
+                            $rangeKeyType = array_key_exists('S', $item[$rangeKeyName]) ? 'S' : 'N';
+                            $key[$rangeKeyName] = array($rangeKeyType => $item[$rangeKeyName][$rangeKeyType]);
+                        }
+                        $this->client->deleteItem(array(
+                            'TableName' => $table,
+                            'Key' => $key
+                        ));
+                }
+        }        
 }
