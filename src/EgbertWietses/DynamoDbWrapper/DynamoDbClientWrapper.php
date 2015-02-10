b481b96986a361c2b13408e8a3c6eecf89fe5632
Patrick Hoogkamer <patrickhoogkamer@gmail.com>
2015-02-10 13:24:16 +0100

    Set up correct folder structure.

---
+++ src/EgbertWietses/DynamoDbWrapper/DynamoDbClientWrapper.php
@@ -0,0 +1,64 @@
+<?php namespace EgbertWietses\DynamoDbWrapper;
+
+use Aws\DynamoDb\DynamoDbClient;
+
+/**
+ * Class DynamoDbClientWrapper
+ */
+class DynamoDbClientWrapper {
+
+    private $client;
+
+    /**
+     * @param $region
+     * @param $key
+     * @param $secret
+     */
+    public function __construct($region, $key, $secret)
+    {
+        $this->client = DynamoDbClient::factory([
+            'region' => $region,
+            'key'    => $key,
+            'secret' => $secret
+        ]);
+    }
+
+    public function emptyTable($table)
+    {
+        // Get table info
+        $result = $this->client->describeTable(['TableName' => $table]);
+        $keySchema = $result['Table']['KeySchema'];
+
+        foreach ($keySchema as $schema)
+        {
+            if($schema['KeyType'] === 'HASH')
+            {
+                $hashKeyName = $schema['AttributeName'];
+            }
+            else if($schema['KeyType'] === 'RANGE')
+            {
+                $rangeKeyName = $schema['AttributeName'];
+            }
+        }
+        // Delete items in the table
+        $scan = $this->client->getIterator('Scan', ['TableName' => $table]);
+        foreach ($scan as $item)
+        {
+            // set hash key
+            $hashKeyType = array_key_exists('S', $item[$hashKeyName]) ? 'S' : 'N';
+            $key = [
+                $hashKeyName => [$hashKeyType => $item[$hashKeyName][$hashKeyType]],
+            ];
+            // set range key if defined
+            if(isset($rangeKeyName))
+            {
+                $rangeKeyType = array_key_exists('S', $item[$rangeKeyName]) ? 'S' : 'N';
+                $key[$rangeKeyName] = [$rangeKeyType => $item[$rangeKeyName][$rangeKeyType]];
+            }
+            $this->client->deleteItem([
+                'TableName' => $table,
+                'Key'       => $key
+            ]);
+        }
+    }
+}
